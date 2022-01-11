a5d76f112a32ddc2aa50e3bc791e52f0f476c451
maxwillerds <max.n.willers@gmail.com>
2022-01-11 15:10:56 +0100

    visualisatie3

--- chips.py
+++ chips.py
@@ -1,14 +1,29 @@
 import grid
+import sys
+import pandas as pd
+import argparse
+
+def main(netlist_file, print_file):
+    netlist = pd.read_csv(netlist_file)
+    print_grid = pd.read_csv(print_file)
+    print(netlist)
+    print(print_grid)
+
 
 if __name__ == "__main__":
 
-    from sys import argv
+    
+    # Set-up parsing command line arguments
+    parser = argparse.ArgumentParser(description="make chip circuit")
 
-    # Check command line arguments
-    if len(argv) is not 1:
-        print("Usage: python3 chips.py [file]")
-        exit(1)
+    # Adding arguments
+    parser.add_argument("netlist_file", help="input file (csv)")
+    parser.add_argument("print_file", help="input print file (csv)")
+   
+    # Read arguments from command line
+    args = parser.parse_args()
 
-    # Load the requested game or else Tiny
-      
-    #filename = f"data/{game_name}Adv.dat"
+    # Run main with provide arguments
+    main(args.netlist_file, args.print_file) 
+    
+
