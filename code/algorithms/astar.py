cc8e3a72d497683c01b53a6ccb056eb6f0332641
Nikki van der Woord <nikkivanderwoord@gmail.com>
2022-01-24 11:11:14 +0100

    Adjusting code to fit our general code structure

--- code/algorithms/astar.py
+++ code/algorithms/astar.py
@@ -123,3 +123,58 @@ def algorithm(grid, start, end):
     f_score = {spot: float("inf") for row in grid for spot in row}
     f_score[start] = getDistance
 
+
+
+
+
+# data structure that handles elements in order from a high to a low assigned priority
+import copy
+from code.classes.net import Net
+from queue import PriorityQueue
+
+
+class Astars(object):
+    """Base class that stores all the required components for a funcioning A* algorithm"""
+
+    def __init__(self, chip, value, parent, start_gate = 0, end_gate = 0):
+        self.chip = copy.deepcopy(chip)
+        self.create_netlist()
+
+                # Initialize parameters
+        self.parent = parent
+        self.value = value
+
+        # Initalize placeholder for the distance 
+        self.distance = 0
+
+        # Create a list to store all neighbouring possibilities
+        self.neighbours = []
+
+        # Check if the current position has a parent
+        if parent:
+            self.start_gate = parent.start_gate
+            self.end_gate = parent.goal
+
+            # Copy the parent path's list to our current one in order to keep track of where we're at
+            self.path = parent.path[:]
+            self.path.append(value)
+
+        # No parent yet, so we create a list for the path and store our starting values
+        else:
+            self.path = [value]
+            self.start = start_gate
+            self.goal = end_gate
+
+
+    def create_netlist(self):
+        """Go over all the connections that need to be made and ensure that they are made"""
+        for i in range(len(self.chip.netlist[0])):
+            self.add_connection(self.chip.gates[self.chip.netlist[0][i]-1], self.chip.gates[self.chip.netlist[1][i]-1])
+
+        
+        
+        # Create a net on the chip
+        net = Net(path)
+        start_gate.connections.append(end_gate.id)
+        end_gate.connections.append(start_gate.id)
+        self.chip.nets.append(net)
