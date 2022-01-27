912308cabdb163e10064d7ae709c25320ffad71a
Madelief Verburg <madelief.verburg@student.uva.nl>
2022-01-27 14:20:00 +0100

    value update

--- code/classes/chips.py
+++ code/classes/chips.py
@@ -5,6 +5,7 @@ from code.classes.net import Net
 from code.classes.gate import Gate
 import pandas as pd
 
+
 class Chip:
     """Class for creating chip"""
 
@@ -34,14 +35,15 @@ class Chip:
             y = gate.y
             z = 0
             self.grid[x][y][z] = -1
-
             self.gates.append(gate)
             
 
     def available_neighbors(self, coordinates):
         """checks available neighbours for each position"""
-        gate_neighbors = []
         good_neighbors = []
+        gate_neighbors = []
+        intersect_neighbors = []
+
      
         x, y, z = coordinates
       
@@ -54,25 +56,40 @@ class Chip:
                 # If the location on the grid is free, it is a option or so-called "good neighbour"
                 if self.grid[x + i][y][z] == 0:      
                     good_neighbors.append((x + i, y, z))
+
                 
                 # If the location on the grid is a gate, it might be the endpoint 
                 elif self.grid[x + i][y][z] == -1:
                     gate_neighbors.append((x + i, y, z))
-            
+                else:
+                    wire = self.grid[x + i][y][z]
+                    if coordinates in wire:
+                        intersect_neighbors.append((x + i, y, z))
+                    
+
             if y + i >= 0 and y + i <= self.length:
                 if self.grid[x][y + i][z] == 0:
                     good_neighbors.append((x, y + i, z))
                 elif self.grid[x][y + i][z] == -1:
                     gate_neighbors.append((x, y + i, z))
+                else:
+                    wire = self.grid[x][y + i][z]
+                    if coordinates in wire:
+                        intersect_neighbors.append((x, y + i, z))
             
             if z + i >= 0 and z + i <= self.height:
                 if self.grid[x][y][z + i] == 0:
                     good_neighbors.append((x, y, z + i))
                 elif self.grid[x][y][z + i] == -1:
                     gate_neighbors.append((x, y, z + i))
+                else:
+                    wire = self.grid[x][y][z + i]
+                    if coordinates in wire:
+                        intersect_neighbors.append((x, y, z + i))
+
         
         # return list of tuples of all possible neighbours 
-        return good_neighbors, gate_neighbors
+        return good_neighbors, gate_neighbors, intersect_neighbors
 
     
     def get_violations(self):
@@ -84,6 +101,7 @@ class Chip:
                     if self.grid[x][y][z] > 1:
                         violations.append((x, y, z))
         return violations
+
 
     def is_solution(self):
         """Returns True if all gates in netlist are connected"""
@@ -100,12 +118,9 @@ class Chip:
         intersections = 0 
         for x in range(self.width):
             for y in range(self.length):
-                counter = 0
                 for z in range(self.height):
-                    if self.grid[x][y][z] > 0:
-                        counter += 1
-                if counter > 1:
-                    intersections = intersections + (counter - 1)
+                    if len(self.grid[x][y][z]) > 1:
+                        intersections += 1
         return intersections
 
     def calculate_value(self):
@@ -130,11 +145,10 @@ class Chip:
         return pd.DataFrame(data = {'net': nets, 'wires' : wires})
 
     def cost(self, location, neighbor):
-        if neighbor[2] > location[2]:
-            self.weights[neighbor] = 1 + 300 * self.intersection(neighbor)
-        else:
-            self.weights[neighbor] = 300 * self.intersection(neighbor)
-       
+        choose, gates = self.available_neighbors(neighbor)
+            
+        self.weights[neighbor] = 300 * self.intersection(neighbor) 
+
         return self.weights[neighbor]
 
 
@@ -147,3 +161,5 @@ class Chip:
         if counter > 1:
             intersections = intersections + (counter - 1)
         return intersections
+
+
